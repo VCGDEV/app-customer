30aae4d196146f1e50bae2378cfd37c83e60b291
Victor de la Cruz <vcg_cruz@hotmail.com>
2020-01-25 21:42:42 -0600

    write customer mapper including testing

--- src/main/java/com/example/customer/model/Customer.java
+++ src/main/java/com/example/customer/model/Customer.java
@@ -1,29 +1,17 @@
 package com.example.customer.model;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="customer")
+@Data
 public class Customer {
     @Id
     private String uuid;
     private String name;
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    private String email;
 }
